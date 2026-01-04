b29821629940e40dfa84d8f67eb002d1c6a36823
Muhsin Chaudhary <znac174@live.rhul.ac.uk>
2026-01-04 23:08:47 +0000

    Added DatabaseConnection class

---
+++ scripts/DatabaseConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+    private static final String Database_path = "jdbc:postgresql://localhost:5432/airline_db";
+    private static final String user = "postgres";
+    private static final String password = "Salah5244.";
+
+    public static Connection getDatabaseConnection(){ // returns the database connection
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(Database_path,user,password);
+        }
+        catch(SQLException e){
+            // Couldnt connect to database 
+        }
+        return connection;
+    } 
+}
