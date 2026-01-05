dd8c32bff28e44d620ce2a5561ea7919bc6ac06f
Muhsin Chaudhary <znac174@live.rhul.ac.uk>
2026-01-05 14:30:40 +0000

    Updated UI for the login page

--- scripts/LoginPage.java
+++ scripts/LoginPage.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,19 +8,26 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
 
 public class LoginPage extends Application {
 
     @Override
     public void start(Stage stage){
+        // Setting up the UI
         Label title = new Label("Airline Reservation System");
+        title.setStyle("-fx-font-weight: bold; -fx-font-size: 20px; -fx-text-fill: black;");
+        title.setMaxWidth(Double.MAX_VALUE);      
+        title.setAlignment(Pos.CENTER);  
         TextField usernameField = new TextField();
         usernameField.setPromptText("Username");
         PasswordField passwordField = new PasswordField();
         passwordField.setPromptText("Password");
         Button loginButton = new Button("Login");
-        Label messageLabel = new Label();
+        Label messageLabel = new Label(); 
 
+        // Logic to be run upon clicking the login button
         loginButton.setOnAction(e -> {
             String username = usernameField.getText();
             String password = passwordField.getText();
@@ -34,8 +42,9 @@ public class LoginPage extends Application {
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
         layout.getChildren().addAll(title,usernameField,passwordField,loginButton,messageLabel);
-
-        Scene scene = new Scene(layout,300,250);
+        VBox.setMargin(usernameField, new Insets(60, 0, 0, 0)); // ensures some distance between title and the other elements
+        // Ensuring that we display the UI 
+        Scene scene = new Scene(layout,600,550);
         stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
