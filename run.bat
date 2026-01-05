9f28829ac1b2b592b4502c7a6c4bb8862dac9d91
Muhsin Chaudhary <znac174@live.rhul.ac.uk>
2026-01-05 23:22:00 +0000

    Made easier to run program with postgreSQL driver and JavaFX by adding run.bat

---
+++ run.bat
@@ -0,0 +1,12 @@
+@echo off
+rem ------------------------------
+rem Compile all Java files
+rem ------------------------------
+javac --module-path C:\openjfx-21.0.9_windows-x64_bin-sdk\javafx-sdk-21.0.9\lib --add-modules javafx.controls scripts\*.java
+
+rem ------------------------------
+rem Run the app with JavaFX + PostgreSQL driver
+rem ------------------------------
+java --module-path C:\openjfx-21.0.9_windows-x64_bin-sdk\javafx-sdk-21.0.9\lib --add-modules javafx.controls -cp "scripts;libraries\postgresql-42.7.8.jar" LoginPage
+
+pause
